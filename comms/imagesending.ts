ce85fb00dcfbd35dc8c772301e499c82b96a7e06
ElectronicMouse <54068147+ElectronicMouse@users.noreply.github.com>
2022-04-24 12:52:17 +0200

    Update and rename sleep.ts to imagesending.ts

--- comms/imagesending.ts
+++ comms/imagesending.ts
@@ -2,13 +2,14 @@ import { ICommand } from "wokcommands";
 import { MessageEmbed } from "..";
 export default {
 category: "Commands",
-description: "answers needed",
+description: "sends image as embed",
 
   callback: ({message}) => {
     const exampleEmbed = new MessageEmbed()
     .setColor('#0099ff')
-    .setTitle('I should sleep but')
-    .setImage('https://i.imgur.com/EHcaBpB.gif')
+    .setTitle('here goes your title')
+    .setImage('image url/path')
     message.reply({ embeds: [exampleEmbed] });
     }  
 } as ICommand
+//learn more about embeds from discord.js documentation
