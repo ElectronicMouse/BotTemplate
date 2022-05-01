ae9ac4c94672b0deda23f9523002d63ccc455ac5
ElectronicMouse <54068147+ElectronicMouse@users.noreply.github.com>
2022-05-01 20:29:19 +0200

    Update clear.ts

--- comms/clear.ts
+++ comms/clear.ts
@@ -1,14 +1,17 @@
 import { ICommand } from "wokcommands";
 export default {
    category: "Commands",
-   description: "shows info or timeschedule of subject",
-   expectedArgs: "<počet>",
+   description: "clears number of messages",
+   expectedArgs: "<number>",
    minArgs: 1,
    maxArgs: 1,
+   //sets which permissions are needed to use it
    permissions: ["ADMINISTRATOR"],
 
    callback: async ({ message, interaction, channel, args }) => {
+      //changes argument to number, also can be done up after expectedArgs as shown in wokcommands documentation
       let number1 = args.length ? parseInt(args.shift()!) : 2
+      //sets limit on how many can be deleted at once
       if(number1>80)
       {
       number1 = 80
